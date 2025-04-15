09db620d3957aea420530d1ebc43c9dcee18a9e8
AlexisJVilc <alexisjunior0911@gmail.com>
2025-04-15 16:55:04 -0400

    Add files via upload

--- test.py
+++ test.py
@@ -1,17 +1,37 @@
 import json
 import boto3
-dynamodb = boto3.resource('dynamodb')
-table = dynamodb.Table('alexisjvilches-resume')
+from botocore.exceptions import NoCredentialsError, PartialCredentialsError
+
+try:
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table('alexisjvilches-resume')
+except (NoCredentialsError, PartialCredentialsError) as e:
+    raise e
+except Exception as e:
+    raise e
+
 def lambda_handler(event, context):
-    response = table.get_item(Key={
-        'id':'1'
-    })
-    views = response['Item']['views']
-    views = views + 1
-    print(views)
-    response = table.put_item(Item={
-        'id':'1',
-        'views':views
-    })
-    
-    return views
+    try:
+        response = table.get_item(Key={'id': '1'})
+        if 'Item' not in response:
+            raise KeyError("El elemento con 'id=1' no existe en la tabla.")
+
+        views = response['Item']['views']
+        views += 1
+
+        table.put_item(Item={'id': '1', 'views': views})
+
+        return {
+            'statusCode': 200,
+            'body': json.dumps({'views': views})
+        }
+    except KeyError as e:
+        return {
+            'statusCode': 400,
+            'body': json.dumps({'error': str(e)})
+        }
+    except Exception as e:
+        return {
+            'statusCode': 500,
+            'body': json.dumps({'error': 'Error interno del servidor'})
+        }
