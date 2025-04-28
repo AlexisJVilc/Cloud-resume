bea8286976b2a0b19a3ce89e538ff44e6d094580
AlexisJVilc <alexisjunior0911@gmail.com>
2025-04-28 19:31:57 -0400

    Update app.py

--- src/app.py
+++ src/app.py
@@ -1,25 +1,39 @@
 import boto3
 import os
+import json
 from boto3.dynamodb.conditions import Key
 
 dynamodb = boto3.resource('dynamodb')
-table = dynamodb.Table(os.environ['HITS_TABLE_NAME'])
+table_name = os.getenv('HITS_TABLE_NAME', None)
+if not table_name:
+    raise ValueError("Environment variable 'HITS_TABLE_NAME' is not set.")
+
+primary_key = os.getenv('PRIMARY_KEY', None)
+if not primary_key:
+    raise ValueError("Environment variable 'PRIMARY_KEY' is not set.")
+
+table = dynamodb.Table(table_name)
 
 def lambda_handler(event, context):
     try:
-        # Increment the hit counter for the resume
         response = table.update_item(
-            Key={'id': os.environ['PRIMARY_KEY']},
+            Key={'id': primary_key},
             UpdateExpression='ADD visits :incr',
             ExpressionAttributeValues={':incr': 1},
             ReturnValues='UPDATED_NEW'
         )
         return {
             'statusCode': 200,
-            'body': f"Updated visits count to {response['Attributes']['visits']}"
+            'body': json.dumps({
+                'message': "Visits count updated successfully.",
+                'updated_visits': response['Attributes']['visits']
+            })
         }
     except Exception as e:
+        print(f"Error: {str(e)}")
         return {
             'statusCode': 500,
-            'body': f"Error: {str(e)}"
-        }
+            'body': json.dumps({
+                'error': "An error occurred while updating visits count.",
+                '
+
